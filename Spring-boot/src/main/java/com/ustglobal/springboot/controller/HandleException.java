81e4c032d4df29de6a53c8732dab8f8178da427c
deepeshdk <deepeshkhandelwal05@gmail.com>
2019-12-07 12:35:34 +0530

    Spring-boot

---
+++ Spring-boot/src/main/java/com/ustglobal/springboot/controller/HandleException.java
@@ -0,0 +1,19 @@
+package com.ustglobal.springboot.controller;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.ustglobal.springboot.dto.EmployeeResponse;
+
+@RestControllerAdvice
+public class HandleException {
+
+	@ExceptionHandler(Exception.class)
+	public EmployeeResponse getException() {
+		EmployeeResponse response = new EmployeeResponse();
+		response.setStatusCode(501);
+		response.setMessage("Error in code");
+		response.setDescription("Got an Exception");
+		return response;
+	}
+} //It is also a ReST controller
